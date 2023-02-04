7f1fc031c93eaec1b6761f00c74f47dd82defd01
Paulina <>
2023-02-04 21:57:51 +0100

    Add new react app

---
+++ react-router-jobs/src/pages/Home.js
@@ -0,0 +1,24 @@
+export default function Home () {
+    return (
+        <div className="home">
+            <h2> Welcome</h2>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam venenatis ut tortor at elementum. 
+            Phasellus cursus pretium egestas. Nam convallis imperdiet ultrices. Morbi rutrum nunc eget quam pulvinar tincidunt. 
+            Sed turpis augue, vulputate et mi ac, posuere cursus ex. In consectetur dictum lectus vel pellentesque. 
+            Mauris feugiat, sem vitae sollicitudin scelerisque, orci metus consequat tellus, ac ornare ex dui id ligula. 
+            Cras id euismod enim. Vestibulum eu cursus arcu, ultricies interdum magna. 
+            Vestibulum mattis rutrum justo, et auctor elit viverra suscipit. </p>
+            <p>Aliquam ac felis est. Cras ex metus, mattis sed rutrum quis, laoreet ut lorem. 
+            Praesent lacus nulla, pharetra sed iaculis nec, semper id lorem. Aliquam malesuada tempus tortor, at imperdiet leo luctus ut.
+            Sed eu nibh neque. In ut enim mi. Mauris sed elit varius, finibus libero ut, commodo lectus. 
+            In lacus felis, porta ac risus dignissim, consequat imperdiet lorem. 
+            Nam molestie diam ut lorem varius, sit amet rutrum nunc tempor. 
+            Nunc vehicula nisi ut massa posuere ultrices. </p>
+            <p>Etiam condimentum purus at dui ullamcorper, ut vestibulum ligula pretium. 
+            Morbi sed tellus ornare, hendrerit arcu non, accumsan libero. Suspendisse tristique sed lorem nec accumsan.
+            Suspendisse non lectus egestas, dapibus turpis et, feugiat erat.
+            </p>
+        </div>
+    )
+}
